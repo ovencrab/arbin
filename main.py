a0fcfa6446a8915796a3303fb239d750123aedca
Jack <jack.evans@materials.ox.ac.uk>
2020-11-13 15:25:16 +0000

    tidied code - added instructions for future code

--- main.py
+++ main.py
@@ -31,9 +31,10 @@ folder_select = 1
 # Outputs
 save_indexed = 0
 save_decimated = 0
-save_cycle_data = 0
-save_processed = 0
-save_cycle_data_converted = 0
+#save_cycle_data = 1
+save_voltage = [1,'mass']
+save_cycle_data_combined = 1
+save_cycle_data_individual = [1,'mass']
 
 # Data processing
 user_decimate = 0
@@ -42,7 +43,7 @@ drop_list = ['test_time','charge_cumulative','discharge_cumulative',
             'charge_energy','discharge_energy','ACR','int_resistance','dv/dt']
 
 # Plot config
-user_plot_fprofile = 1
+user_plot_fprofile = 0
 user_cell = 1
 user_cycle = 2 # 0 plots all cycles # '2-5' to plot cycles 2 to 5
 user_x = 'test_time'
@@ -88,7 +89,7 @@ except :
 
 # Load csv data
 tic = time.perf_counter()
-df, n_cells, raw = load.csv(data_paths, cell_info)
+df_raw, n_cells, raw = load.csv(data_paths, cell_info)
 toc = time.perf_counter()
 print(f"Imported files in {toc - tic:0.1f}s")
 
@@ -97,7 +98,7 @@ print('--------------------------------------')
 print("Returned data:")
 print('--------------------------------------')
 [print(key, ': ', value) for key, value in cell_info.items()]
-print('Dataframe size: {}'.format(df.size))
+print('Dataframe size: {}'.format(df_raw.size))
 
 #------------------------
 # Data filtering
@@ -110,7 +111,7 @@ if raw :
 
     tic = time.perf_counter()
     # Call index function on raw dataframe
-    df = filt.index(df)
+    df_indexed = filt.index(df_raw)
     toc = time.perf_counter()
     print(f"4 - Created new index in {toc - tic:0.1f}s")
 
@@ -118,7 +119,7 @@ if raw :
     if save_indexed == 1 :
         tic = time.perf_counter()
 
-        message, success = save.multi(df, data_folder, data_paths, 'indexed', 'indexed')
+        message, success = save.multi(df_indexed, data_folder, data_paths, 'indexed', 'indexed')
         cell_info['filtered'] = 1
         save.yml(cell_info, 'indexed', data_folder, info_path)
 
@@ -140,17 +141,17 @@ if user_decimate == 1 :
 
     tic = time.perf_counter()
     # Call decimate function on dataframe
-    df_decimate, counter = filt.decimate(df, row_target)
+    df_decimate, counter = filt.decimate(df_indexed, row_target)
     toc = time.perf_counter()
     t = f'{toc - tic:0.1f}'
-    print('{} - Reduced number of rows from {} to {} in {}s'.format(counter, df.shape[0], df_decimate.shape[0],t))
-    df = df_decimate
+    print('{} - Reduced number of rows from {} to {} in {}s'.format(counter, df_indexed.shape[0], df_decimate.shape[0],t))
+    df_indexed = df_decimate
 
     # Save decimated data to csv files in */decimated folder
     if save_decimated == 1 :
         tic = time.perf_counter()
 
-        message, success = save.multi(df, data_folder, data_paths, 'decimated', 'decimated')
+        message, success = save.multi(df_indexed, data_folder, data_paths, 'decimated', 'decimated')
         cell_info['decimated'] = 1
         save.yml(cell_info, 'decimated', data_folder, info_path)
 
@@ -162,7 +163,6 @@ if user_decimate == 1 :
         else :
             print(message)
 
-
 #------------------------
 # Capacity calculations
 #------------------------
@@ -173,7 +173,7 @@ print('--------------------------------------')
 
 tic = time.perf_counter()
 # Call function to filter data into capacity per cycle
-df_cap, df = filt.cap(df, n_cells)
+df_cyc, df_processed = filt.cap(df_indexed, n_cells)
 
 # Update cell_info with new average cell
 cell_info['cell'].insert(0, 0)
@@ -185,50 +185,57 @@ toc = time.perf_counter()
 print(f"1 - Subtractive capacity and cycle data generated in {toc - tic:0.1f}s")
 
 # Save cycle data to csv files in */output folder
-if save_cycle_data == 1 :
-    tic = time.perf_counter()
-    message, success = save.single(df_cap, data_folder, data_paths, cell_info, 'output', 'cycle_data')
-    toc = time.perf_counter()
-    if success == 1 :
-        print(f"2 - Saved formatted cycle data to '*/output' in {toc - tic:0.1f}s")
-    else :
-        print(message)
+#if save_cycle_data == 1 :
+#    tic = time.perf_counter()
+#    message, success = save.single(df_cap, data_folder, data_paths, cell_info, 'output', 'cycle_data')
+#    toc = time.perf_counter()
+#    if success == 1 :
+#        print(f"2 - Saved formatted cycle data to '*/output' in {toc - tic:0.1f}s")
+#    else :
+#        print(message)
+
+print('--------------------------------------')
+print('Convert voltage profiles to alternative formats')
+print('--------------------------------------')
+
+df_voltage = df_processed.drop(columns=drop_list)
+
+###### <------ check save_voltage[1] and convert p_cap and n_cap based on the selected param
 
 # Save processed voltage profile data to csv files in */output folder
-if save_processed == 1 :
+if save_voltage[0] == 1 :
     tic = time.perf_counter()
-    df_export = df.drop(columns=drop_list)
-    message, success = save.multi(df_export, data_folder, data_paths, 'output', 'processed')
+    message, success = save.multi(df_voltage, data_folder, data_paths, 'output', 'voltage')
     toc = time.perf_counter()
     if success == 1 :
-        print(f"3 - Saved processed voltage profile data to '*/output' in {toc - tic:0.1f}s")
+        print(f"2 - Saved indexed voltage profile data to '*/output' in {toc - tic:0.1f}s")
     else :
         print(message)
 
 print('--------------------------------------')
-print('Convert capacity data to other formats')
+print('Convert cycle data to other formats')
 print('--------------------------------------')
 
 tic = time.perf_counter()
 
 # Create multi index in column axis referring to 'raw' capacity
-df_cap = pd.concat([df_cap], axis=1, keys=['raw'])
-df_cap.sort_index(inplace=True)
-df_cap = filt.cap_convert(df_cap, cell_info, 'mAh', ['p_cap', 'n_cap', 'p_cap_std', 'n_cap_std'])
+df_cyc = pd.concat([df_cyc], axis=1, keys=['raw'])
+df_cyc.sort_index(inplace=True)
+df_cyc = filt.cap_convert(df_cyc, cell_info, 'mAh', ['p_cap', 'n_cap', 'p_cap_std', 'n_cap_std'])
 
 # For each cell in data frame, convert mAh to mAh/g (cell = 0 is the mean)
-df_cap_mass = filt.cap_convert(df_cap, cell_info, 'mass', ['p_cap', 'n_cap', 'p_cap_std', 'n_cap_std'])
+df_cyc_mass = filt.cap_convert(df_cyc, cell_info, 'mass', ['p_cap', 'n_cap', 'p_cap_std', 'n_cap_std'])
 
 # For each cell in data frame, convert mAh to mAh/cm3 (cell = 0 is the mean)
-df_cap_vol = filt.cap_convert(df_cap, cell_info, 'volume', ['p_cap', 'n_cap', 'p_cap_std', 'n_cap_std'])
+df_cyc_vol = filt.cap_convert(df_cyc, cell_info, 'volume', ['p_cap', 'n_cap', 'p_cap_std', 'n_cap_std'])
 
 # For each cell in data frame, convert mAh to mAh/cm2 (cell = 0 is the mean)
-df_cap_areal = filt.cap_convert(df_cap, cell_info, 'areal', ['p_cap', 'n_cap', 'p_cap_std', 'n_cap_std'])
+df_cyc_areal = filt.cap_convert(df_cyc, cell_info, 'areal', ['p_cap', 'n_cap', 'p_cap_std', 'n_cap_std'])
 
 # Concatenate raw, mass and vol cycle capacity dataframes
-df_cap = pd.concat([df_cap, df_cap_mass,  df_cap_areal, df_cap_vol], axis=1)
+df_cyc = pd.concat([df_cyc, df_cyc_mass,  df_cyc_areal, df_cyc_vol], axis=1)
 
-df_cap_reformat = filt.reformat(df_cap)
+df_cyc_reformat = filt.reformat(df_cyc)
 
 #df_cap_reformat.loc[idx[:],idx[:,param,:]]
 
@@ -236,9 +243,9 @@ toc = time.perf_counter()
 
 print(f"1 - Converted cycle data to mass, areal and volume format in {toc - tic:0.1f}s")
 
-if save_cycle_data_converted == 1 :
+if save_cycle_data_combined == 1 :
     tic = time.perf_counter()
-    message, success = save.param_filter(df_cap_reformat, data_folder, data_paths, cell_info, 'output')
+    message, success = save.param_filter(df_cyc_reformat, data_folder, data_paths, cell_info, 'output')
     toc = time.perf_counter()
     if success == 1 :
         print(f"2 - Saved formatted cycle data to '*/output' in {toc - tic:0.1f}s")
@@ -256,7 +263,7 @@ print('--------------------------------------')
 # Plot voltage/current profiles
 if user_plot_fprofile == 1 :
     tic = time.perf_counter()
-    fig = plot_data.fprofile(df, user_cell, user_cycle, user_x, user_y, user_y2)
+    fig = plot_data.fprofile(df_voltage, user_cell, user_cycle, user_x, user_y, user_y2)
     fig.show()
     toc = time.perf_counter()
     print(f"Plot generated in {toc - tic:0.1f}s")
@@ -266,21 +273,21 @@ if user_plot_cycle == 1 :
     tic = time.perf_counter()
 
     # Checks for the number of cells in the data frame and plots traces
-    if len(df_cap.index.get_level_values(0).unique()) == 1 :
+    if len(df_cyc.index.get_level_values(0).unique()) == 1 :
         # Dataframe only contains 1 cell
         fig = go.Figure()
-        cell_idx = df_cap.index.get_level_values(0).unique().values[0]
-        fig = plot_data.cycle_trace(df_cap, fig, cell_idx, user_cap_param, user_cyc_y)
+        cell_idx = df_cyc.index.get_level_values(0).unique().values[0]
+        fig = plot_data.cycle_trace(df_cyc, fig, cell_idx, user_cap_param, user_cyc_y)
         plot_data.def_format(fig, cell_idx, n_cells)
     else :
         fig = go.Figure()
-        for cell_idx in df_cap.index.get_level_values(0).unique() :
+        for cell_idx in df_cyc.index.get_level_values(0).unique() :
             if cell_idx == 0 :
                 # skip cell index of 0 as it is the mean
                 continue
             else :
                 # create trace for all other cell indexes
-                fig = plot_data.cycle_trace(df_cap, fig, cell_idx, user_cap_param, user_cyc_y)
+                fig = plot_data.cycle_trace(df_cyc, fig, cell_idx, user_cap_param, user_cyc_y)
 
         # Plot traces for each cell
         plot_data.def_format(fig, cell_idx, n_cells)
@@ -288,7 +295,7 @@ if user_plot_cycle == 1 :
         # Plot trace of mean
         fig_avg = go.Figure()
         cell_idx = 0
-        fig_avg = plot_data.cycle_trace(df_cap.xs(0, level='cell', drop_level=False), fig_avg, cell_idx, user_cap_param, user_cyc_y)
+        fig_avg = plot_data.cycle_trace(df_cyc.xs(0, level='cell', drop_level=False), fig_avg, cell_idx, user_cap_param, user_cyc_y)
         plot_data.def_format(fig_avg, cell_idx, n_cells)
 
     toc = time.perf_counter()
@@ -296,5 +303,6 @@ if user_plot_cycle == 1 :
 
 #Debug
 #idx = pd.IndexSlice
-#df_cap.loc[idx[1, 1], idx['raw', 'p_curr']]
+#df_cyc.loc[idx[1, 1], idx['raw', 'p_curr']]
+#df_cyc_reformat.loc[idx[:], idx[:,'mAh', ('p_cap','p_cap_std')]]
 
